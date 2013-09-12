e55970b28e995f1489bb4029c00fdb9546e966a7
berlin <ilya.kolodnik@gmail.com>
2013-09-12 11:37:07 +0300

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Studentime::Application.config.secret_key_base = '51a631fa4fc44c60f2e39f8e92273d31bdf9af9a706b7fa0cbf7c0fb728bc8967cda2425fedb7705f277b7f88d09b48c60ef03796f25ba5389201a5a39b385b9'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Studentime::Application.config.session_store :cookie_store, key: '_studentime_session'
