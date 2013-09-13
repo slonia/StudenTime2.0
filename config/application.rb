cc58e99888ca910cb4a1969739e16da493ea570f
berlin <ilya.kolodnik@gmail.com>
2013-09-13 21:30:06 +0300

    add some useful gems

--- config/application.rb
+++ config/application.rb
@@ -19,5 +19,9 @@ module Studentime
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
+    config.generators do |gen|
+      gen.helper false
+      gen.assets false
+    end
   end
 end
